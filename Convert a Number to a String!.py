f098f9b5a4d6cd9c8e61ffd7940ab8610c55d096
tfchan-rob <77974597+tfchan-rob@users.noreply.github.com>
2021-05-22 14:46:48 +0800

    Add files via upload

---
+++ Convert a Number to a String!.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Convert a Number to a String!
+
+# We need a function that can transform a number into a string.
+# 
+# What ways of achieving this do you know?
+# 
+# Examples:
+
+# 123 --> "123"
+# 
+# 999 --> "999"
+
+# ## my solution
+
+# In[2]:
+
+
+def number_to_string(num):
+    
+    return str(num)
+
+
+# In[ ]:
+
+
+
+
